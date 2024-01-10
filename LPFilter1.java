f415b3d76ed87003df0e9c2aaf168dc9700956e0
MehdiBENSMAIL <mehdi.ben_smail@edu.univ-fcomte.fr>
2024-01-10 13:01:45 +0100

    Header + javadoc

--- LPFilter1.java
+++ LPFilter1.java
@@ -1,8 +1,8 @@
 // coding : UTF-8
 // Date : 11/01/2024
-// Auteurs : Jean-Baptiste FROEHLY   - B2
+// Authors : Jean-Baptiste FROEHLY   - B2
 //           Mehdi         BEN SMAIL - C1
-// Filtre passe-bas de moyenne glissante
+// Low-pass filter moving average
 
 public class LPFilter1 {
     public static void main(String[] args) {
@@ -10,24 +10,24 @@ public class LPFilter1 {
     }
 
     /**
-     * Filtre passe-bas de moyenne glissante
-     * @param audio tableau d'entrée (double)
-     * @param n nombre d'échantillons à moyenner (int)
+     * Apply a low pass filter to the audio array (moving average)
+     * @param audioInput the input audio array (double[])
+     * @param sampleCount the number of samples to average
      */
-    public void audioLPFilter(double[] audio, int n) {
-        // On crée le tableau de sortie
-        double[] output = new double[audio.length];
-        // On parcourt le tableau de sortie
-        for (int i = 0; i < output.length; i++) {
-          // On calcule la moyenne des échantillons
-          double sum = 0;
-          for (int j = 0; j < n; j++) {
-            if (i - j >= 0) {
-              sum += audio[i - j];
-            }
+    public void audioLPFilter(double[] audioInput, int sampleCount) {
+      // Create the output array
+      double[] output = new double[audioInput.length];
+      // Iterate over the output array
+      for (int i = 0; i < output.length; i++) {
+        // Calculate the average of the samples
+        double sum = 0;
+        for (int j = 0; j < sampleCount; j++) {
+          if (i - j >= 0) {
+            sum += audioInput[i - j];
           }
-          output[i] = sum / n;
         }
-        audio = output;
+        output[i] = sum / sampleCount;
+      }
+      audioInput = output;
     }
 }
