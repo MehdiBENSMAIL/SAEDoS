caf8f81a5c6bbd32eafa7e5b15a94baf1fc7eb06
MehdiBENSMAIL <mehdi.ben_smail@edu.univ-fcomte.fr>
2024-01-10 13:05:11 +0100

    Update LPFilter2.java

--- LPFilter2.java
+++ LPFilter2.java
@@ -1,3 +1,9 @@
+// coding : UTF-8
+// Date : 11/01/2024
+// Authors : Jean-Baptiste FROEHLY   - B2
+//           Mehdi         BEN SMAIL - C1
+// Low-pass filter Fast Fourier Transform (FFT)
+
 public class LPFilter2 {
     // Filtre passe-bas basé sur fourier
 
@@ -12,7 +18,12 @@ public class LPFilter2 {
         audioLPFilter(audio, 10);
     }
 
-    public static void audioLPFilter(double[] audio, int n) {
+    /**
+     * Apply a low pass filter to the audio array (FFT)
+     * @param audioInput the input audio array (double[])
+     * @param sampleCount the number of samples to average
+     */
+    public static void audioLPFilter(double[] audio, int sampleCount) {
         // On calcule la transformée de Fourier
         Complex[] fourier = FFT.fft(audio);
         // On crée un tableau de sortie
@@ -21,12 +32,12 @@ public class LPFilter2 {
         for (int i = 0; i < output.length; i++) {
             // On calcule la moyenne des échantillons
             double sum = 0;
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < sampleCount; j++) {
                 if (i - j >= 0) {
                     sum += fourier[i - j].re();
                 }
             }
-            output[i] = new Complex(sum / n, 0);
+            output[i] = new Complex(sum / sampleCount, 0);
         }
         // On calcule la transformée de Fourier inverse
         Complex[] inverse = FFT.ifft(output);
